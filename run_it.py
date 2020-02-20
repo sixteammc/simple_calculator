0b075391fdcfcf33366f4a780dd41506a72802c9
radek-coder <rradek441@gmail.com>
2020-02-20 12:19:55 -0800

    fixed silly isue

--- run_it.py
+++ run_it.py
@@ -1,19 +1,19 @@
-from addition import addition as add
-from multiplication import multiplication as m
-from division import division as d
-from subtraction import subtraction as s
+from addition import addition
+from multiplication import multiplication
+from division import division
+from subtraction import subtraction
 
-num_1 = input("Please insert your first number: ")
-num_2 = input("Please insert your second number: ")
-operation = input("Please insert your operation")
+num_1 = int(input("Please insert your first number: "))
+num_2 = int(input("Please insert your second number: "))
+operation = input("Please insert your operation ")
 
 if operation == "x" or operation == "X" or operation == "*":
-    print(m(num_1, num_2))
+    print(multiplication(num_1, num_2))
 elif operation == "+":
-    print(add(num_1, num_2))
+    print(addition(num_1, num_2))
 elif operation == "-":
-    print(s(num_1, num_2))
+    print(subtraction(num_1, num_2))
 elif operation == "/":
-    print(d(num_1, num_2))
+    print(division(num_1, num_2))
 else:
     print("You are trying to break my calculator")
